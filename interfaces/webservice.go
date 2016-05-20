dae7964c01b51ed7f48697f69f130511bc165972
arzonus <vsevolod.kaloshin@gmail.com>
2016-05-20 17:34:38 +0300

    change logs

--- interfaces/webservice.go
+++ interfaces/webservice.go
@@ -21,6 +21,8 @@ func (handler WebserviceHandler) GetCVEById(res http.ResponseWriter, req *http.R
 
 func (handler WebserviceHandler) GetListCVEByProduct(res http.ResponseWriter, req *http.Request) {
 	handler.Logger.Info("Get List CVE By Product")
+	handler.Logger.Info(req.URL)
+
 	name := getParamFromURL("name", req)
 	version := getParamFromURL("version", req)
 	product := d.Product{
@@ -34,6 +36,7 @@ func (handler WebserviceHandler) GetListCVEByProduct(res http.ResponseWriter, re
 	}
 
 	data, err := json.Marshal(jCVE{CVEs: cves})
+	handler.Logger.Info(data)
 
 	if err != nil {
 		handler.Logger.Info(err)
